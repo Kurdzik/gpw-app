a472f08eec0c0058818088b68dd97af967dc5cd1
Mateusz <mateuszkurdziel1@gmail.com>
2022-09-15 19:28:31 +0200

    added remove credentials option

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from templates.python_scripts.db_functions import get_data_from_db, test_db_conn
 import pandas as pd
 import sqlalchemy
 
+engine = sqlalchemy.create_engine("postgresql://j341:ED1F_a359b0@psql01.mikr.us:5432/db_j341")
+conn = engine.connect()
+
 download_ready = ''
 approval = False
 conn_string = ''
@@ -11,13 +14,14 @@ db_schema = ''
 
 app = Flask(__name__,template_folder='./templates',static_folder='./templates/static')
 
+#----------------- HOME PAGE -------------------------------------------------------------------------------
 @app.route('/')
 def main():
 
     return render_template('index.html')
 
 
-#----------------- RAW DATA DOWNLOAD --------------------------------------------------------
+#----------------- RAW DATA DOWNLOAD -----------------------------------------------------------------------
 @app.route('/get_data')
 def init_data_loader():
     global download_ready
@@ -48,13 +52,12 @@ def run_data_loader():
 
     return render_template('index_scrapper.html',download_ready=download_ready,file_name=file_name)
 
-#----------------- ANALYTICS MODULE --------------------------------------------------------
-@app.route('/analytics')
-def init_model():
-
-    return render_template('index_analytics.html')
-
 #----------------- CONNECT WITH EXTERNAL DB MODULE --------------------------------------------------------
+@app.route('/db_choice')
+def init_choice():
+
+    return render_template('index_db_conn_choice.html')
+
 @app.route('/db_conn')
 def init_conn():
     message = ''
@@ -79,8 +82,6 @@ def test_conn():
     add_record_button = ''
     wrong_conn_string = ''
 
-    # message =1
-    # message_link =2
     message, message_link, conn_string, approval = test_db_conn(db_type=db_type,
                                                                 db_ip=db_ip,
                                                                 db_port=db_port,
@@ -103,22 +104,82 @@ def test_conn():
                                                 wrong_conn_string=wrong_conn_string,
                                                 add_record_button=add_record_button
                                                 ),approval
-                                                # ,
-
 
 @app.route('/add_connection',methods=['POST','GET'])
 def add_conn():
 
-    # conn = sqlite3.connect('templates/static/db_credentials/creds_DB.db')
-    engine = sqlalchemy.create_engine("postgresql://j341:ED1F_a359b0@psql01.mikr.us:5432/db_j341")
-    conn = engine.connect()
+
     
     try:pd.DataFrame([conn_string,db_table,db_schema,'active']).T.rename({0:'connection_string',1:'table',2:'schema',3:'status'},axis=1).to_sql('connections',if_exists='append',con=conn,schema='connected_dbs',index=False)
-    except Exception: print('error')
-    message = 'Connection Added Successfully'
+    except Exception: message = 'Something went wrong, connection was not added'
+    message = 'Connection added successfully'
 
 
     return render_template('index_db_conn.html',message=message)
+
+@app.route('/find_existing_conn',methods=['POST','GET'])
+def init_conn_removal():
+    
+    return render_template('index_db_conn_remove.html')
+
+@app.route('/confirmation',methods=['POST','GET'])
+def confirm_removal():
+    global conn_string
+
+    db_type = request.form['selectDatabases']
+    db_ip = request.form['DBIP']
+    db_port = request.form['DBPort']
+    database = request.form['DB']
+    db_user = request.form['DBUser']
+    db_pass = request.form['DBPass']
+
+
+    message, message_link, conn_string, approval = test_db_conn(db_type=db_type,
+                                                                db_ip=db_ip,
+                                                                db_port=db_port,
+                                                                database=database,
+                                                                db_user=db_user,
+                                                                db_pass=db_pass)
+    
+    # conn = sqlite3.connect('templates/static/db_credentials/creds.db')
+    q = 'select * from connected_dbs.connections'
+    df = pd.read_sql(q,conn)
+
+    if len(df.loc[df.connection_string==conn_string])==0:
+        message = 'Connection not in a database'
+    else:
+        message = 'Connection found!'
+    
+    remove_record_button = 'Click here to remove your Database from registry'
+
+
+    return render_template('index_db_conn_remove.html',message=message,remove_record_button=remove_record_button)
+
+
+@app.route('/remove_conn',methods=['POST','GET'])
+def remove_conn():
+    global conn_string
+
+    message = ''
+
+    print('connection string:',conn_string)
+
+    q = 'select * from connected_dbs.connections'
+    df = pd.read_sql(q,conn)
+
+    df = df.drop(df.loc[df.connection_string==conn_string].index)
+
+    df.to_sql('connections',if_exists='replace',con=conn, schema='connected_dbs', index=False)
+
+    message = 'Connection removed successfully'
+
+    return render_template('index_db_conn_remove.html',message=message)
+
+#----------------- ANALYTICS MODULE --------------------------------------------------------
+@app.route('/analytics')
+def init_model():
+
+    return render_template('index_analytics.html')
 
 
 if __name__ == '__main__':
